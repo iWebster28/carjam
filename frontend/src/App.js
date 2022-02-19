46323b099b67890e8769121fd4a52d91a9f9a6b0
David Song <david990724@gmail.com>
2022-02-19 17:09:43 -0500

    Adding micinput in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Test from './components/Test';
+import MicInput from './components/MicInput';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       </header>
 
       <Test />
+      <MicInput/>
 
     </div>
   );
