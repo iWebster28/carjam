06db16ea3ee3136797df13b5ff3bcce836f1d96f
Tong Yin <71139027+TongYinn@users.noreply.github.com>
2022-02-19 21:54:00 -0500

    Added Frontend Stuff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,29 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 
 import Test from './components/Test';
+import RecordButton from './components/RecordButton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="title">
+          CarJam
+        </h1>
+
+        <h3 className="tagline">
+          Tagline
+        </h3>
       </header>
 
-      <Test />
+
+      <RecordButton />
+      
 
     </div>
   );
 }
 
 export default App;
+
+//<img src={logo} className="App-microphone" alt="microphone" />
